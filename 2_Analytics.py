4e534992812af47617090656c37c8756a1864a0e
GOWRI MANOGAR SJ <143477659+Gowrimanogar@users.noreply.github.com>
2025-12-25 22:42:00 +0530

    Update 2_Analytics.py

--- 2_Analytics.py
+++ 2_Analytics.py
@@ -1,9 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("📊 Report Analytics")
 
-if "df" not in st.session_state:
+if "lab_data" not in st.session_state:
     st.warning("Upload a lab report first from Dashboard")
 else:
-    df = st.session_state["df"]
+    df = pd.DataFrame(st.session_state["lab_data"])
     st.bar_chart(df.set_index("Test")["Value"])
